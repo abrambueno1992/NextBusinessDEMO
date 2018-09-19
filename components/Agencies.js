1aac515104f2af548d77188596db974b4c5300f4
Abraham Bueno <abueno@localhost.localdomain>
2018-09-19 00:15:27 -0500

    Fetched data with getInitialProps, which is an improvement from CDM.

--- components/Agencies.js
+++ components/Agencies.js
@@ -18,16 +18,7 @@ export default class extends React.Component {
 
 
   render() {
-    return <div id="agencies">
-      <h2>Most trusted agencies in Stockholm</h2>
-      <p>We the brand asked for offers for our Sample Real Project and had an extra round of design fixes - that's our
-        way of ranking these agencies.</p>
-      {(this.state.agencies && this.state.agencies.length > 0) ?
-        <ul>
-          {this.state.agencies.map(agency => <li><h3>{agency.title}</h3></li>)}
-        </ul> : <p><strong>Have nothing</strong></p>}
-      <hr/>
-    </div>
+    return
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,37 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
-import Agencies from '../components/Agencies'
+// import Agencies from '../components/Agencies'
 import Information from '../components/Information'
+import 'isomorphic-fetch'
+class Index extends React.Component {
+  static async getInitialProps() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const agencies = await response.json()
+    return {agencies}
+  }
+  render() {
+    const agencies = this.props.agencies
+    return <Layout>
+        <Hero/>
+        {/*<Agencies/>*/}
+        <div id="agencies">
+          <h2>Most trusted agencies in Stockholm</h2>
+          <p>We the brand asked for offers for our Sample Real Project and had an extra round of design fixes - that's our
+            way of ranking these agencies.</p>
+          {(agencies && agencies.length > 0) ?
+            <ul>
+              {agencies.map(agency => <li key={agency.id}><h3>{agency.title}</h3></li>)}
+            </ul> : <p><strong>Have nothing</strong></p>}
+          <hr/>
+        </div>
+        <Information/>
 
-const Index = () => (
-  <Layout>
-      <Hero/>
-      <Agencies/>
-      <Information/>
-  </Layout>
-)
+      </Layout>
+
+  }
+}
+
+
 
 export default Index
